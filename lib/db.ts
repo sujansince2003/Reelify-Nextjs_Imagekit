7ace284170e33e6b713ea5486c1e97a60a5d5df3
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-02-15 09:41:21 +0545

    feat: implement MongoDB connection helper with caching

---
+++ lib/db.ts
@@ -0,0 +1,49 @@
+//connection to database
+
+import mongoose from "mongoose";
+
+const mongoURL = process.env.MONGODB_URL!;
+
+if (!mongoURL) {
+    throw new Error("Mongodb connection URL not provided ")
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const options =
+        {
+            bufferCommands: true,
+            maxPoolSize: 10,
+
+        };
+        cached.promise = mongoose.connect(mongoURL, options).then((mongoose) => {
+            return mongoose.connection;
+        })
+
+
+    }
+
+
+    try {
+        cached.conn = await cached.promise;
+
+    } catch (error) {
+        cached.promise = null
+        throw error;
+
+    }
+    return cached.conn
+
+}
+
+export { dbConnect }
